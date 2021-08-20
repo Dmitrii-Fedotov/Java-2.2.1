7a1316e60230c71ae0e2a317e4560b174301faf3
Dmitrii Fedotov <fedotovdmz@gmail.com>
2021-08-20 13:11:59 +0300

    initial comit

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
         int miles = service.calculate(price);
         System.out.println(miles);
 
-        int miles1 = service.calculate(2_543);
-        System.out.println(miles1);
-
-        int miles2 = service.calculate(6_244);
-        System.out.println(miles2);
-
-        int miles3 = service.calculate(87_000);
-        System.out.println(miles3);
+//        int miles1 = service.calculate(2_543);
+//        System.out.println(miles1);
+//
+//        int miles2 = service.calculate(6_244);
+//        System.out.println(miles2);
+//
+//        int miles3 = service.calculate(87_000);
+//        System.out.println(miles3);
     }
 }
